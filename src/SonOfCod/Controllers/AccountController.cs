88c61bbb342e827a09edc9a164f242cd4470ed39
Chad Durkin <Chaddurkin@gmail.com>
2017-04-28 14:36:51 -0700

    no more roles

--- src/SonOfCod/Controllers/AccountController.cs
+++ src/SonOfCod/Controllers/AccountController.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SonOfCod.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
 using SonOfCod.ViewModels;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using SonOfCod.Models;
+
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,6 +18,7 @@ namespace SonOfCod.Controllers
 {
     public class AccountController : Controller
     {
+        private IHostingEnvironment _environment;
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
